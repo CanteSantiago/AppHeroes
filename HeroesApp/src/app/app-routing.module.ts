4e41d954711574726cbe5177b78729bd270e7253
Santiago Cante <114036508+CanteSantiago@users.noreply.github.com>
2023-11-21 12:56:52 -0500

    Vid182-[Seccion13]-HeroesApp
    Inicio de un nuevo proyecto

---
+++ HeroesApp/src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule} from '@angular/router';
+
+const Routes:Routes=[
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(Routes)],
+  exports: [RouterModule],
+
+})
+export class AppRoutingModule { }
