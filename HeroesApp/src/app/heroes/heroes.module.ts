210fc14c358fef31ccfd853b3c48138b3147e566
Santiago Cante <114036508+CanteSantiago@users.noreply.github.com>
2023-11-22 21:29:58 -0500

    Vid193-Material Sidevar,Toolbar y icons

--- HeroesApp/src/app/heroes/heroes.module.ts
+++ HeroesApp/src/app/heroes/heroes.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { HeroesRoutingModule } from './heroes-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HeroPageComponent } from './pages/hero-page/hero-page.component';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { ListPageComponent } from './pages/list-page/list-page.component';
 import { NewPageComponent } from './pages/new-page/new-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
-
+import { HeroesRoutingModule } from './heroes-routing.module';
+import { MaterialModule } from '../material/material.module';
 
 @NgModule({
   declarations: [
@@ -15,11 +15,16 @@ import { SearchPageComponent } from './pages/search-page/search-page.component';
     LayoutPageComponent,
     ListPageComponent,
     NewPageComponent,
-    SearchPageComponent
+    SearchPageComponent,
+
+
+
   ],
   imports: [
     CommonModule,
-    HeroesRoutingModule
+    ReactiveFormsModule,
+    HeroesRoutingModule,
+    MaterialModule,
   ]
 })
 export class HeroesModule { }
